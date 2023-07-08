1dfbbba93186f90fe8af142221d87f70a968c1fa
Robert Manders <robert.manders@gmail.com>
2023-07-08 23:55:57 +0100

    Code block.

---
+++ components/CodeBlock.vue
@@ -0,0 +1,22 @@
+<template>
+	<pre class="pre"><code class="language-css">.class {
+	property: value
+}</code></pre>
+</template>
+
+<script setup>
+import Prism from 'prismjs'
+import '../styles/prism-atom-dark.css'
+import 'prismjs/components/prism-css'
+
+onMounted(() => {
+	Prism.highlightAll()
+})
+</script>
+
+<style scoped>
+.pre {
+	tab-size: 2;
+	border-radius: 10px;
+}
+</style>
